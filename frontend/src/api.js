85d79988077847c44619c7bc4f87383e673b65f8
ElpiDim <elpida.el.dimitriadou@gmail.com>
2026-04-08 19:51:05 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const baseURL = (process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL.replace(/\/+$/, '') : '') + '/api';
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + '/api'
+  baseURL
 });
 
 // interceptor
